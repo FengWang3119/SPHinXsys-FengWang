bb058b2282ac816447b403d3341ae066c6c6d0a9
Feng Wang <feng.wang.aer@tum.de>
2024-10-30 17:35:08 +0100

    case re-configuration

--- tests/extra_source_and_tests/19/19.h
+++ tests/extra_source_and_tests/19/19.h
@@ -5,33 +5,40 @@
  * @author 	Xiangyu Hu, Chi Zhang and Luhui Han
  */
 
-#include "k-epsilon_turbulent_model.cpp"
+#include "bidirectional_buffer.h"
+#include "density_correciton.h"
+#include "density_correciton.hpp"
+#include "k-omega_turbulent_model.cpp"
+#include "kernel_summation.h"
+#include "kernel_summation.hpp"
+#include "pressure_boundary.h"
 #include "sphinxsys.h"
 using namespace SPH;
 
 //----------------------------------------------------------------------
 //	Basic geometry parameters and numerical setup.
 //----------------------------------------------------------------------
-Real DH = 2.0; /**< Channel height. */
-Real extend_in = 2.0;
-Real extend_out = 4.0;
-Real DL1 = 1.0 + extend_in;
-Real DL2 = 1.5;
-Real DL3 = 1.0;
-Real DL4 = 1.5;
-Real DL5 = 1.0 + extend_out;
-Real DL = DL1 + DL2 + DL3 + DL4 + DL5;
-Real DH1 = DL2 / sqrt(3.0);
+Real DH = 2.0;  /**< Channel height. */
+Real DL = 30.0; /**< Channel length. */
+Real num_fluid_cross_section = 20.0;
 
-Real num_fluid_cross_section = 100.0;
 //----------------------------------------------------------------------
 //	Unique parameters for turbulence.
 //----------------------------------------------------------------------
 Real characteristic_length = DH; /**<It needs characteristic Length to calculate turbulent length and the inflow turbulent epsilon>*/
+//** For K and Epsilon, type of the turbulent inlet, 0 is freestream, 1 is from interpolation from PY21 *
+int type_turbulent_inlet = 1;
+Real relaxation_rate_turbulent_inlet = 0.8;
+//** Tag for AMRD *
+int is_AMRD = 0;
+//** Weight for correcting the velocity  gradient in the sub near wall region  *
+Real weight_vel_grad_sub_nearwall = 0.1;
+//** Tag for Source Term Linearisation *
+bool is_source_term_linearisation = false;
 //** Initial values for K, Epsilon and Mu_t *
 StdVec<Real> initial_turbu_values = {0.000180001, 3.326679e-5, 1.0e-9};
 
-Real y_p_constant = 0.025;
+Real y_p_constant = 0.05;
 Real resolution_ref = (DH - 2.0 * y_p_constant) / (num_fluid_cross_section - 1.0); /**< Initial reference particle spacing. */
 Real offset_distance = y_p_constant - resolution_ref / 2.0;                        //** Basically offset distance is large than or equal to 0 *
 
@@ -46,30 +53,16 @@ BoundingBox system_domain_bounds(Vec2d(-DL_sponge - 2.0 * BW, -BW), Vec2d(DL + 2
 //----------------------------------------------------------------------
 //	Material properties of the fluid.
 //----------------------------------------------------------------------
-//** Laminar *
-//Real U_inlet = 0.5;
-//Real U_max = 0.75;
-//Real U_f = U_inlet; //*Characteristic velo is regarded as average velo here
-//Real c_f = 10.0 * U_max;                                        /**< Speed of sound. */
-//Real rho0_f = 1.0;                                            /**< Density. */
-//Real mu_f = 0.01;
-
-//** Same parameter as SPH_4 *
-//Real U_inlet = 1.0;
-//Real U_f = U_inlet; //*Characteristic velocity
-//Real U_max = 1.5 * U_inlet; //** An estimated value, generally 1.5 U_inlet *
-//Real c_f = 10.0 * U_max;
-//Real rho0_f = 1.0;                                            /**< Density. */
-//Real Re = 40000.0;
-//Real mu_f = rho0_f * U_f * DH / Re;
-
-//** Same parameter as SPH_9 *
 Real U_inlet = 1.0;
-Real U_f = U_inlet; //*Characteristic velocity
-Real U_max = 3.0 * U_inlet;
-Real c_f = 10.0 * U_max; /**< Speed of sound. */
-Real rho0_f = 1.0;       /**< Density. */
-Real mu_f = 0.00005;
+Real U_f = U_inlet;         //*Characteristic velocity
+Real U_max = 1.5 * U_inlet; //** An estimated value, generally 1.5 U_inlet *
+Real c_f = 10.0 * U_max;
+Real rho0_f = 1.0; /**< Density. */
+Real Re = 20000.0;
+
+Real Outlet_pressure = 0.0;
+
+Real mu_f = rho0_f * U_f * DH / Re;
 
 Real Re_calculated = U_f * DH * rho0_f / mu_f;
 
@@ -77,24 +70,26 @@ Real DH_C = DH - 2.0 * offset_distance;
 //----------------------------------------------------------------------
 //	The emitter block with offset model.
 //----------------------------------------------------------------------
-Vec2d emitter_halfsize = Vec2d(0.5 * BW, 0.5 * DH_C + BW);
-Vec2d emitter_translation = Vec2d(-DL_sponge, 0.0) + emitter_halfsize + Vecd(0.0, offset_distance - BW);
-Vec2d inlet_buffer_halfsize = Vec2d(0.5 * DL_sponge, 0.5 * DH_C + BW);
-Vec2d inlet_buffer_translation = Vec2d(-DL_sponge, 0.0) + inlet_buffer_halfsize + Vecd(0.0, offset_distance - BW);
+Vec2d left_buffer_halfsize = Vec2d(0.5 * BW, 0.5 * DH_C + BW);
+Vec2d left_buffer_translation = Vec2d(-DL_sponge, 0.0) + left_buffer_halfsize + Vecd(0.0, offset_distance - BW);
 
-Vec2d disposer_halfsize = Vec2d(0.5 * BW, 0.75 * DH);
-Vec2d disposer_translation = Vec2d(DL, DH + 0.25 * DH) - disposer_halfsize;
+Vec2d right_buffer_halfsize = Vec2d(0.5 * BW, 0.75 * DH);
+Vec2d right_buffer_translation = Vec2d(DL, DH + 0.25 * DH) - right_buffer_halfsize;
 //----------------------------------------------------------------------
 // Observation with offset model.
 //----------------------------------------------------------------------
-Real x_observe = 5.0;
-//Real x_observe_start = 0.90 * DL;
-//Real observe_spacing_x = 0.02 * DL;
-//int num_observer_points_x = 1;
+Real x_observe_start = 0.99 * DL;
 int num_observer_points = std::round(DH_C / resolution_ref); //**Every particle is regarded as a cell monitor*
-StdVec<Real> monitoring_bound = {x_observe - 1.0 * resolution_ref, x_observe + 1.0 * resolution_ref};
-//Real observe_spacing = DH_C / num_observer_points;
-//StdVec<Vecd> observation_locations;
+Real observe_spacing = DH_C / num_observer_points;
+
+// By kernel weight.
+StdVec<Vecd> observation_location;
+Vecd pos_observe_start = Vecd(x_observe_start, resolution_ref / 2.0 + offset_distance);
+Vecd unit_direction_observe = Vecd(0.0, 1.0);
+Real observer_offset_distance = 2.0 * resolution_ref;
+
+//** For regression test *
+StdVec<Vecd> observer_location_center_point = {Vecd(0.5 * DL, 0.5 * DH)};
 //----------------------------------------------------------------------
 //	Cases-dependent geometries
 //----------------------------------------------------------------------
@@ -105,12 +100,6 @@ std::vector<Vecd> createWaterBlockShape()
     water_block_shape.push_back(Vecd(-DL_sponge - offset_distance, DH));
     water_block_shape.push_back(Vecd(DL + offset_distance, DH));
     water_block_shape.push_back(Vecd(DL + offset_distance, 0.0));
-
-    water_block_shape.push_back(Vecd(DL - DL5, 0.0));
-    water_block_shape.push_back(Vecd(DL1 + DL2 + DL3, DH1));
-    water_block_shape.push_back(Vecd(DL1 + DL2, DH1));
-    water_block_shape.push_back(Vecd(DL1, 0.0));
-
     water_block_shape.push_back(Vecd(-DL_sponge - offset_distance, 0.0));
     return water_block_shape;
 }
@@ -131,12 +120,6 @@ std::vector<Vecd> createOuterWallShape()
     water_block_shape.push_back(Vecd(-DL_sponge - BW, DH));
     water_block_shape.push_back(Vecd(DL + BW, DH));
     water_block_shape.push_back(Vecd(DL + BW, 0.0));
-
-    water_block_shape.push_back(Vecd(DL - DL5, 0.0));
-    water_block_shape.push_back(Vecd(DL1 + DL2 + DL3, DH1));
-    water_block_shape.push_back(Vecd(DL1 + DL2, DH1));
-    water_block_shape.push_back(Vecd(DL1, 0.0));
-
     water_block_shape.push_back(Vecd(-DL_sponge - BW, 0.0));
 
     return water_block_shape;
@@ -148,12 +131,6 @@ std::vector<Vecd> createInnerWallShape()
     water_block_shape.push_back(Vecd(-DL_sponge - 2.0 * BW, DH));
     water_block_shape.push_back(Vecd(DL + 2.0 * BW, DH));
     water_block_shape.push_back(Vecd(DL + 2.0 * BW, 0.0));
-
-    water_block_shape.push_back(Vecd(DL - DL5, 0.0));
-    water_block_shape.push_back(Vecd(DL1 + DL2 + DL3, DH1));
-    water_block_shape.push_back(Vecd(DL1 + DL2, DH1));
-    water_block_shape.push_back(Vecd(DL1, 0.0));
-
     water_block_shape.push_back(Vecd(-DL_sponge - 2.0 * BW, 0.0));
 
     return water_block_shape;
@@ -198,9 +175,49 @@ struct InflowVelocity
         //target_velocity[0] = 1.5 * u_ave * SMAX(0.0, 1.0 - position[1] * position[1] / halfsize_[1] / halfsize_[1]);
         //target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
         target_velocity[0] = u_ave;
+        if (1)
+        {
+            //** Impose fully-developed velocity from PYTHON result */
+            //** Calculate the distance to wall, Y. position[1] is the distance to the centerline */
+            Real Y = half_channel_height - std::abs(position[1]);
+            int polynomial_order = 8;
+            int num_coefficient = polynomial_order + 1;
+            //** Coefficient of the polynomial, 8th-order, from py21 dp=0.024 */
+            // Real coeff[] = {
+            //     6.153336e-01, 3.095679e+00, -1.399783e+01,
+            //     4.798221e+01, -1.100147e+02, 1.619762e+02,
+            //     -1.464631e+02, 7.373006e+01, -1.577924e+01
+            // };
+            //** Coefficient of the polynomial, 8th-order, from py21 dp=0.1 */
+            Real coeff[] = {
+                6.492006e-01, 2.145673e+00, -7.442681e+00,
+                2.148624e+01, -4.443593e+01, 6.171458e+01,
+                -5.439313e+01, 2.726584e+01, -5.887918e+00};
+            Real polynomial_value = 0.0;
+            for (int i = 0; i < num_coefficient; ++i)
+            {
+                polynomial_value += coeff[i] * std::pow(Y, i);
+            }
+
+            if (Y > half_channel_height || Y < 0.0)
+            {
+                std::cout << "position[1]=" << position[1] << std::endl;
+                std::cout << "Y=" << Y << std::endl;
+                std::cout << "polynomial_value=" << polynomial_value << std::endl;
+                std::cout << "Stop" << std::endl;
+                std::cout << "=================" << std::endl;
+                std::cin.get();
+            }
+
+            //** Impose inlet velocity gradually */
+            target_velocity[0] = run_time < t_ref_ ? 0.5 * polynomial_value * (1.0 - cos(Pi * run_time / t_ref_)) : polynomial_value;
+            //target_velocity[0] = polynomial_value;
+        }
+
         if (position[1] > half_channel_height)
         {
             std::cout << "Particles out of domain, wrong inlet velocity." << std::endl;
+            std::cout << position[1] << std::endl;
             std::cin.get();
         }
         target_velocity[1] = 0.0;
@@ -223,5 +240,28 @@ class TimeDependentAcceleration : public Gravity
         Real run_time_ = GlobalStaticVariables::physical_time_;
         du_ave_dt_ = 0.5 * u_ref_ * (Pi / t_ref_) * sin(Pi * run_time_ / t_ref_);
         return run_time_ < t_ref_ ? Vecd(du_ave_dt_, 0.0) : global_acceleration_;
+    }
+};
+
+struct RightOutflowPressure
+{
+    template <class BoundaryConditionType>
+    RightOutflowPressure(BoundaryConditionType &boundary_condition) {}
+
+    Real operator()(Real &p_)
+    {
+        /*constant pressure*/
+        Real pressure = Outlet_pressure;
+        return pressure;
+    }
+};
+struct LeftInflowPressure
+{
+    template <class BoundaryConditionType>
+    LeftInflowPressure(BoundaryConditionType &boundary_condition) {}
+
+    Real operator()(Real &p_)
+    {
+        return p_;
     }
 };
