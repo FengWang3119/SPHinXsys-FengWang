ab8ad31a4236322e55cd907094b107d3263aff54
Feng Wang <feng.wang.aer@tum.de>
2024-10-30 10:24:05 +0100

    0

--- tests/extra_source_and_tests/18/18.cpp
+++ tests/extra_source_and_tests/18/18.cpp
@@ -133,9 +133,6 @@ int main(int ac, char *av[])
 
     InteractionWithUpdate<LinearGradientCorrectionMatrixComplex> corrected_configuration_fluid(water_block_inner, water_wall_contact);
 
-    //InteractionWithUpdate<LinearGradientCorrectionMatrixInner> corrected_configuration_fluid(water_block_inner);
-    InteractionWithUpdate<fluid_dynamics::TurbulentLinearGradientCorrectionMatrixInner> corrected_configuration_fluid_only_inner(water_block_inner);
-
     /** Pressure relaxation algorithm with Riemann solver for viscous flows. */
     //Dynamics1Level<fluid_dynamics::Integration1stHalfWithWallRiemann> pressure_relaxation(water_block_inner, water_wall_contact);
     //Dynamics1Level<fluid_dynamics::Integration1stHalfCorrectionWithWallRiemann> pressure_relaxation(water_block_inner, water_wall_contact);
@@ -146,30 +143,12 @@ int main(int ac, char *av[])
     //Dynamics1Level<fluid_dynamics::Integration2ndHalfWithWallRiemann> density_relaxation(water_block_inner, water_wall_contact);
     //Dynamics1Level<fluid_dynamics::Integration2ndHalfWithWall<DissipativeRiemannSolver>> density_relaxation(water_block_inner, water_wall_contact);
 
-    /** Turbulent.Note: When use wall function, K Epsilon calculation only consider inner */
-    InteractionWithUpdate<fluid_dynamics::JudgeIsNearWall> update_near_wall_status(water_block_inner, water_wall_contact);
-
-    InteractionWithUpdate<fluid_dynamics::GetVelocityGradientInner> get_velocity_gradient(water_block_inner, weight_vel_grad_sub_nearwall);
-    //InteractionWithUpdate<fluid_dynamics::GetVelocityGradientComplex> get_velocity_gradient(water_block_inner, water_wall_contact);
-    //InteractionWithUpdate<fluid_dynamics::VelocityGradientWithWall<LinearGradientCorrection>> vel_grad_calculation(water_block_inner, water_wall_contact);
-
-    InteractionWithUpdate<fluid_dynamics::K_TurbulentModelInner> k_equation_relaxation(water_block_inner, initial_turbu_values, is_AMRD, is_source_term_linearisation);
-    InteractionWithUpdate<fluid_dynamics::E_TurbulentModelInner> epsilon_equation_relaxation(water_block_inner, is_source_term_linearisation);
-    InteractionDynamics<fluid_dynamics::TKEnergyForceComplex> turbulent_kinetic_energy_force(water_block_inner, water_wall_contact);
-    InteractionDynamics<fluid_dynamics::StandardWallFunctionCorrection> standard_wall_function_correction(water_block_inner, water_wall_contact, y_p_constant);
-
-    SimpleDynamics<fluid_dynamics::ConstrainNormalVelocityInRegionP> constrain_normal_velocity_in_P_region(water_block);
-
     /** Choose one, ordinary or turbulent. Computing viscous force, */
-    InteractionWithUpdate<fluid_dynamics::TurbulentViscousForceWithWall> turbulent_viscous_force(water_block_inner, water_wall_contact);
-    //InteractionWithUpdate<fluid_dynamics::ViscousForceWithWall> viscous_force(water_block_inner, water_wall_contact);
+    InteractionWithUpdate<fluid_dynamics::ViscousForceWithWall> viscous_force(water_block_inner, water_wall_contact);
 
     /** Impose transport velocity, with or without limiter . */
-    //InteractionWithUpdate<fluid_dynamics::TransportVelocityLimitedCorrectionComplex<BulkParticles>> transport_velocity_correction(water_block_inner, water_wall_contact);
-    InteractionWithUpdate<fluid_dynamics::TVC_ModifiedLimited_RKGC_OBFCorrection<BulkParticles>> transport_velocity_correction(water_block_inner, water_wall_contact);
-
-    /** A temporarily test for the limiter . */
-    SimpleDynamics<fluid_dynamics::GetLimiterOfTransportVelocityCorrection> get_limiter_of_transport_velocity_correction(water_block, 1000);
+    InteractionWithUpdate<fluid_dynamics::TransportVelocityLimitedCorrectionComplex<BulkParticles>> transport_velocity_correction(water_block_inner, water_wall_contact);
+    //InteractionWithUpdate<fluid_dynamics::TVC_ModifiedLimited_RKGC_OBFCorrection<BulkParticles>> transport_velocity_correction(water_block_inner, water_wall_contact);
 
     /** Evaluation of density by summation approach. */
     //InteractionWithUpdate<fluid_dynamics::DensitySummationFreeStreamComplex> update_density_by_summation(water_block_inner, water_wall_contact);
@@ -190,9 +169,6 @@ int main(int ac, char *av[])
 
     SimpleDynamics<fluid_dynamics::InflowVelocityCondition<InflowVelocity>> inflow_velocity_condition(left_emitter);
 
-    /** Turbulent InflowTurbulentCondition.It needs characteristic Length to calculate turbulent length  */
-    SimpleDynamics<fluid_dynamics::InflowTurbulentCondition> impose_turbulent_inflow_condition(left_emitter, characteristic_length, relaxation_rate_turbulent_inlet, type_turbulent_inlet);
-
     //----------------------------------------------------------------------
     // Right/Outlet buffer
     //----------------------------------------------------------------------
@@ -207,14 +183,11 @@ int main(int ac, char *av[])
     InteractionWithUpdate<fluid_dynamics::DensitySummationPressureComplex> update_fluid_density_pressure(water_block_inner, water_wall_contact);
 
     /** Choose one, ordinary or turbulent. Time step size without considering sound wave speed. */
-    ReduceDynamics<fluid_dynamics::TurbulentAdvectionTimeStepSize> get_turbulent_fluid_advection_time_step_size(water_block, U_f);
-    //ReduceDynamics<fluid_dynamics::AdvectionViscousTimeStep> get_fluid_advection_time_step_size(water_block, U_f);
+    ReduceDynamics<fluid_dynamics::AdvectionViscousTimeStep> get_fluid_advection_time_step_size(water_block, U_f);
 
     /** Time step size with considering sound wave speed. */
     ReduceDynamics<fluid_dynamics::AcousticTimeStep> get_fluid_time_step_size(water_block);
 
-    /** Turbulent eddy viscosity calculation needs values of Wall Y start. */
-    SimpleDynamics<fluid_dynamics::TurbulentEddyViscosity> update_eddy_viscosity(water_block);
     //----------------------------------------------------------------------
     //	Define the configuration related particles dynamics.
     //----------------------------------------------------------------------
@@ -229,11 +202,8 @@ int main(int ac, char *av[])
     body_states_recording.addToWrite<Real>(water_block, "Density");             // output for debug
     body_states_recording.addToWrite<Vecd>(water_block, "ZeroGradientResidue"); // output for debug
     ObservedQuantityRecording<Vecd> write_recorded_water_velocity("Velocity", fluid_observer_contact);
-    ObservedQuantityRecording<Real> write_recorded_water_k("TurbulenceKineticEnergy", fluid_observer_contact);
-    ObservedQuantityRecording<Real> write_recorded_water_mut("TurbulentViscosity", fluid_observer_contact);
-    ObservedQuantityRecording<Real> write_recorded_water_epsilon("TurbulentDissipation", fluid_observer_contact);
     body_states_recording.addToWrite<int>(water_block, "BufferParticleIndicator");
-    RegressionTestDynamicTimeWarping<ObservedQuantityRecording<Real>> write_centerpoint_quantity("TurbulentViscosity", observer_centerpoint_contact);
+    body_states_recording.addToWrite<Real>(water_block, "VolumetricMeasure");
     /**
      * @brief Setup geometry and initial conditions.
      */
@@ -254,7 +224,6 @@ int main(int ac, char *av[])
     Real &physical_time = *sph_system.getSystemVariableDataByName<Real>("PhysicalTime");
     size_t number_of_iterations = sph_system.RestartStep();
     int screen_output_interval = 100;
-    int observation_sample_interval = screen_output_interval * 2;
     Real end_time = 200.0;             /**< End time. */
     Real Output_Time = end_time / 4.0; /**< Time stamps for output of body states. */
     Real dt = 0.0;                     /**< Default acoustic time step sizes. */
@@ -267,7 +236,6 @@ int main(int ac, char *av[])
     //	First output before the main loop.
     //----------------------------------------------------------------------
     body_states_recording.writeToFile();
-    write_centerpoint_quantity.writeToFile(number_of_iterations);
     //----------------------------------------------------------------------------------------------------
     //	Main loop starts here.
     //----------------------------------------------------------------------------------------------------
@@ -280,8 +248,8 @@ int main(int ac, char *av[])
         {
             apply_gravity_force.exec();
 
-            //Real Dt = get_fluid_advection_time_step_size.exec();
-            Real Dt = get_turbulent_fluid_advection_time_step_size.exec();
+            Real Dt = get_fluid_advection_time_step_size.exec();
+            //Real Dt = get_turbulent_fluid_advection_time_step_size.exec();
 
             //inlet_outlet_surface_particle_indicator.exec();
 
@@ -289,15 +257,11 @@ int main(int ac, char *av[])
             update_fluid_density_pressure.exec();
 
             corrected_configuration_fluid.exec();
-            corrected_configuration_fluid_only_inner.exec();
 
-            update_eddy_viscosity.exec();
-
-            //viscous_force.exec();
-            turbulent_viscous_force.exec();
+            viscous_force.exec();
+            //turbulent_viscous_force.exec();
 
             transport_velocity_correction.exec();
-            get_limiter_of_transport_velocity_correction.exec();
 
             /** Dynamics including pressure relaxation. */
             Real relaxation_time = 0.0;
@@ -306,32 +270,17 @@ int main(int ac, char *av[])
             {
                 dt = SMIN(get_fluid_time_step_size.exec(), Dt);
 
-                turbulent_kinetic_energy_force.exec();
-
                 pressure_relaxation.exec(dt);
 
                 kernel_summation.exec();
                 left_inflow_pressure_condition.exec(dt);
                 right_outflow_pressure_condition.exec(dt);
 
-                if (is_constrain_normal_velocity_in_P_region)
-                    constrain_normal_velocity_in_P_region.exec();
-
                 inflow_velocity_condition.exec();
-
-                impose_turbulent_inflow_condition.exec();
 
                 density_relaxation.exec(dt);
 
                 distance_to_wall.exec();
-                update_near_wall_status.exec();
-
-                standard_wall_function_correction.exec();
-
-                get_velocity_gradient.exec(dt);
-
-                k_equation_relaxation.exec(dt);
-                epsilon_equation_relaxation.exec(dt);
 
                 relaxation_time += dt;
                 integration_time += dt;
@@ -348,10 +297,6 @@ int main(int ac, char *av[])
                 std::cout << std::fixed << std::setprecision(9) << "N=" << number_of_iterations << "	Time = "
                           << physical_time
                           << "	Dt = " << Dt << "	dt = " << dt << "\n";
-                if (number_of_iterations % observation_sample_interval == 0 && number_of_iterations != sph_system.RestartStep())
-                {
-                    write_centerpoint_quantity.writeToFile(number_of_iterations);
-                }
             }
             number_of_iterations++;
 
@@ -380,9 +325,6 @@ int main(int ac, char *av[])
             if (physical_time > end_time * 0.6)
             {
                 write_recorded_water_velocity.writeToFile(number_of_iterations);
-                write_recorded_water_k.writeToFile(number_of_iterations);
-                write_recorded_water_mut.writeToFile(number_of_iterations);
-                write_recorded_water_epsilon.writeToFile(number_of_iterations);
             }
             //if (physical_time > end_time * 0.5)
             //body_states_recording.writeToFile();
@@ -401,14 +343,5 @@ int main(int ac, char *av[])
     tt = t4 - t1 - interval;
     std::cout << "Total wall time for computation: " << tt.seconds()
               << " seconds." << std::endl;
-
-    if (sph_system.GenerateRegressionData())
-    {
-        write_centerpoint_quantity.generateDataBase(1.0e-3);
-    }
-    else
-    {
-        write_centerpoint_quantity.testResult();
-    }
     return 0;
 }
--- tests/extra_source_and_tests/18/18.h
+++ tests/extra_source_and_tests/18/18.h
@@ -8,7 +8,7 @@
 #include "bidirectional_buffer.h"
 #include "density_correciton.h"
 #include "density_correciton.hpp"
-#include "k-epsilon_turbulent_model.cpp"
+//#include "k-epsilon_turbulent_model.cpp"
 #include "kernel_summation.h"
 #include "kernel_summation.hpp"
 #include "pressure_boundary.h"
@@ -25,23 +25,8 @@ Real num_fluid_cross_section = 20.0;
 //----------------------------------------------------------------------
 //	Unique parameters for turbulence.
 //----------------------------------------------------------------------
-Real characteristic_length = DH; /**<It needs characteristic Length to calculate turbulent length and the inflow turbulent epsilon>*/
-//** For K and Epsilon, type of the turbulent inlet, 0 is freestream, 1 is from interpolation from PY21 *
-int type_turbulent_inlet = 1;
-Real relaxation_rate_turbulent_inlet = 0.8;
-//** Tag for AMRD *
-int is_AMRD = 0;
-bool is_constrain_normal_velocity_in_P_region = false;
-//** Weight for correcting the velocity  gradient in the sub near wall region  *
-Real weight_vel_grad_sub_nearwall = 0.1;
-//** Tag for Source Term Linearisation *
-bool is_source_term_linearisation = false;
-//** Initial values for K, Epsilon and Mu_t *
-StdVec<Real> initial_turbu_values = {0.000180001, 3.326679e-5, 1.0e-9};
 
-Real y_p_constant = 0.05;
-Real resolution_ref = (DH - 2.0 * y_p_constant) / (num_fluid_cross_section - 1.0); /**< Initial reference particle spacing. */
-Real offset_distance = y_p_constant - resolution_ref / 2.0;                        //** Basically offset distance is large than or equal to 0 *
+Real resolution_ref = DH / num_fluid_cross_section; /**< Initial reference particle spacing. */
 
 Real BW = resolution_ref * 4; /**< Reference size of the emitter. */
 Real DL_sponge = resolution_ref * 20;
@@ -59,7 +44,7 @@ Real U_f = U_inlet;         //*Characteristic velocity
 Real U_max = 1.5 * U_inlet; //** An estimated value, generally 1.5 U_inlet *
 Real c_f = 10.0 * U_max;
 Real rho0_f = 1.0; /**< Density. */
-Real Re = 20000.0;
+Real Re = 200.0;
 
 Real Outlet_pressure = 0.0;
 
@@ -67,7 +52,6 @@ Real mu_f = rho0_f * U_f * DH / Re;
 
 Real Re_calculated = U_f * DH * rho0_f / mu_f;
 
-Real DH_C = DH - 2.0 * offset_distance;
 //----------------------------------------------------------------------
 //	The emitter block with offset model.
 //----------------------------------------------------------------------
@@ -84,12 +68,12 @@ Vec2d right_buffer_translation = Vec2d(DL - 2.5 * resolution_ref, 0.5 * DH);
 // Observation with offset model.
 //----------------------------------------------------------------------
 Real x_observe_start = 0.99 * DL;
-int num_observer_points = std::round(DH_C / resolution_ref); //**Every particle is regarded as a cell monitor*
-Real observe_spacing = DH_C / num_observer_points;
+int num_observer_points = std::round(DH / resolution_ref); //**Every particle is regarded as a cell monitor*
+Real observe_spacing = DH / num_observer_points;
 
 // By kernel weight.
 StdVec<Vecd> observation_location;
-Vecd pos_observe_start = Vecd(x_observe_start, resolution_ref / 2.0 + offset_distance);
+Vecd pos_observe_start = Vecd(x_observe_start, resolution_ref / 2.0);
 Vecd unit_direction_observe = Vecd(0.0, 1.0);
 Real observer_offset_distance = 2.0 * resolution_ref;
 
@@ -101,11 +85,11 @@ StdVec<Vecd> observer_location_center_point = {Vecd(0.5 * DL, 0.5 * DH)};
 std::vector<Vecd> createWaterBlockShape()
 {
     std::vector<Vecd> water_block_shape;
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance, 0.0));
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance, DH));
-    water_block_shape.push_back(Vecd(DL + offset_distance, DH));
-    water_block_shape.push_back(Vecd(DL + offset_distance, 0.0));
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance, 0.0));
+    water_block_shape.push_back(Vecd(-DL_sponge, 0.0));
+    water_block_shape.push_back(Vecd(-DL_sponge, DH));
+    water_block_shape.push_back(Vecd(DL, DH));
+    water_block_shape.push_back(Vecd(DL, 0.0));
+    water_block_shape.push_back(Vecd(-DL_sponge, 0.0));
     return water_block_shape;
 }
 class WaterBlock : public ComplexShape
@@ -114,7 +98,7 @@ class WaterBlock : public ComplexShape
     explicit WaterBlock(const std::string &shape_name) : ComplexShape(shape_name)
     {
         MultiPolygon computational_domain(createWaterBlockShape());
-        add<ExtrudeShape<MultiPolygonShape>>(-offset_distance, computational_domain, "ComputationalDomain");
+        add<ExtrudeShape<MultiPolygonShape>>(0.0, computational_domain, "ComputationalDomain");
     }
 };
 
@@ -150,10 +134,10 @@ class WallBoundary : public ComplexShape
     explicit WallBoundary(const std::string &shape_name) : ComplexShape(shape_name)
     {
         MultiPolygon outer_dummy_boundary(createOuterWallShape());
-        add<ExtrudeShape<MultiPolygonShape>>(-offset_distance + BW, outer_dummy_boundary, "OuterDummyBoundary");
+        add<ExtrudeShape<MultiPolygonShape>>(BW, outer_dummy_boundary, "OuterDummyBoundary");
 
         MultiPolygon inner_dummy_boundary(createInnerWallShape());
-        subtract<ExtrudeShape<MultiPolygonShape>>(-offset_distance, inner_dummy_boundary, "InnerDummyBoundary");
+        subtract<ExtrudeShape<MultiPolygonShape>>(0.0, inner_dummy_boundary, "InnerDummyBoundary");
     }
 };
 
@@ -176,47 +160,8 @@ struct InflowVelocity
     {
         Vecd target_velocity = velocity;
         Real u_ave = current_time < t_ref_ ? 0.5 * u_ref_ * (1.0 - cos(Pi * current_time / t_ref_)) : u_ref_;
-        //target_velocity[0] = 1.5 * u_ave * SMAX(0.0, 1.0 - position[1] * position[1] / halfsize_[1] / halfsize_[1]);
-        //target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
-        target_velocity[0] = u_ave;
-        if (1)
-        {
-            //** Impose fully-developed velocity from PYTHON result */
-            //** Calculate the distance to wall, Y. position[1] is the distance to the centerline */
-            Real Y = half_channel_height - std::abs(position[1]);
-            int polynomial_order = 8;
-            int num_coefficient = polynomial_order + 1;
-            //** Coefficient of the polynomial, 8th-order, from py21 dp=0.024 */
-            // Real coeff[] = {
-            //     6.153336e-01, 3.095679e+00, -1.399783e+01,
-            //     4.798221e+01, -1.100147e+02, 1.619762e+02,
-            //     -1.464631e+02, 7.373006e+01, -1.577924e+01
-            // };
-            //** Coefficient of the polynomial, 8th-order, from py21 dp=0.1 */
-            Real coeff[] = {
-                6.492006e-01, 2.145673e+00, -7.442681e+00,
-                2.148624e+01, -4.443593e+01, 6.171458e+01,
-                -5.439313e+01, 2.726584e+01, -5.887918e+00};
-            Real polynomial_value = 0.0;
-            for (int i = 0; i < num_coefficient; ++i)
-            {
-                polynomial_value += coeff[i] * std::pow(Y, i);
-            }
-
-            if (Y > half_channel_height || Y < 0.0)
-            {
-                std::cout << "position[1]=" << position[1] << std::endl;
-                std::cout << "Y=" << Y << std::endl;
-                std::cout << "polynomial_value=" << polynomial_value << std::endl;
-                std::cout << "Stop" << std::endl;
-                std::cout << "=================" << std::endl;
-                std::cin.get();
-            }
-
-            //** Impose inlet velocity gradually */
-            target_velocity[0] = current_time < t_ref_ ? 0.5 * polynomial_value * (1.0 - cos(Pi * current_time / t_ref_)) : polynomial_value;
-            //target_velocity[0] = polynomial_value;
-        }
+        target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
+        //target_velocity[0] = u_ave;
 
         if (position[1] > half_channel_height)
         {
