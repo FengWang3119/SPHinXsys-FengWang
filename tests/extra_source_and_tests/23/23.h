c60e19134a8043ad56530778c76f20b82ab0e827
Feng Wang <feng.wang.aer@tum.de>
2025-01-12 17:58:35 +0100

    change scale as FLUENT tutorial

--- tests/extra_source_and_tests/23/23.h
+++ tests/extra_source_and_tests/23/23.h
@@ -26,12 +26,13 @@ Real U_f = U_inlet;         //*Characteristic velocity
 Real U_max = 1.5 * U_inlet; //** An estimated value, generally 1.5 U_inlet *
 Real c_f = 10.0 * U_max;
 Real T_ref = 2.0;
-Real rho0_f = 1000.0;
-Real Re = 50.0;
+Real rho0_f = 1.0;
+Real Re = 40.0;
 
 Real Outlet_pressure = 0.0;
 
 Real mu_f = rho0_f * U_f * DH / Re;
+
 Real Re_calculated = U_f * DH * rho0_f / mu_f;
 //----------------------------------------------------------------------
 //	Geometric shapes used in this case.
