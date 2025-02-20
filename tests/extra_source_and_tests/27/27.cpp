78ff0933f08729ac8647211f32fd760b3cce7b94
Feng Wang <feng.wang.aer@tum.de>
2025-02-20 17:19:10 +0100

    3

--- tests/extra_source_and_tests/27/27.cpp
+++ tests/extra_source_and_tests/27/27.cpp
@@ -232,10 +232,10 @@ int main(int ac, char *av[])
     Real &physical_time = *sph_system.getSystemVariableDataByName<Real>("PhysicalTime");
     size_t number_of_iterations = 0;
     int screen_output_interval = 100;
-    Real end_time = 800.0;                      /**< End time. */
+    Real end_time = 80.0;                       /**< End time. */
     Real cutoff_ratio = 0.9;                    //** cutoff_time should be a integral and the same as the PY script */
     Real cutoff_time = cutoff_ratio * end_time; //** cutoff_time should be a integral and the same as the PY script */
-    Real num_output_files = 200.0;
+    Real num_output_files = 20000.0;
     Real Output_Time = end_time / num_output_files; /**< Time stamps for output of body states. */
     Real index_check_file_fully_developed = num_output_files * cutoff_ratio;
     Real dt = 0.0; /**< Default acoustic time step sizes. */
@@ -285,7 +285,7 @@ int main(int ac, char *av[])
             {
                 dt = SMIN(get_fluid_time_step_size.exec(), Dt);
 
-                //turbulent_kinetic_energy_force.exec();
+                turbulent_kinetic_energy_force.exec();
 
                 pressure_relaxation.exec(dt);
 
