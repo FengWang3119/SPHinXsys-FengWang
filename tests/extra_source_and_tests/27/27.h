c2934b1ec9c7cd8f24ff6728583e5cfa829b3f90
Feng Wang <feng.wang.aer@tum.de>
2025-02-26 17:15:29 +0100

    15

--- tests/extra_source_and_tests/27/27.h
+++ tests/extra_source_and_tests/27/27.h
@@ -14,7 +14,7 @@ using namespace SPH;
 //----------------------------------------------------------------------
 Real DH = 2.0; /**< Channel height. */
 Real characteristic_length = DH;
-Real num_fluid_cross_section = 5.0;
+Real num_fluid_cross_section = 10.0;
 //----------------------------------------------------------------------
 //	Resolution for turbulence.
 //----------------------------------------------------------------------
@@ -40,10 +40,10 @@ Real extend_in = 0.0;
 Real extend_out = 0.0;
 Real extend_compensate_relaxation = 0.0;
 
-// Real DH1 = 5.0 * DH;
-// Real DL2 = 10.0 * DH;
-Real DH1 = 20.0 * DH;
-Real DL2 = 50.0 * DH;
+Real DH1 = 5.0 * DH;
+Real DL2 = 10.0 * DH;
+// Real DH1 = 20.0 * DH;
+// Real DL2 = 50.0 * DH;
 
 Vec2d point_O(0.0, 0.0);
 Vec2d point_A = point_O + Vec2d(0.0, DH + 2.0 * DH1);
