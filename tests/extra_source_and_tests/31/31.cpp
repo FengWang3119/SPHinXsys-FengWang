6b60e71f41a6c8cc0c301e65fb3f5fee066b06b5
Feng Wang <feng.wang.aer@tum.de>
2025-03-08 18:05:39 +0100

    recover observer

--- tests/extra_source_and_tests/31/31.cpp
+++ tests/extra_source_and_tests/31/31.cpp
@@ -39,23 +39,23 @@ int main(int ac, char *av[])
     ObserverBody observer_center_point(sph_system, "ObserverCenterPoint");
     observer_center_point.generateParticles<ObserverParticles>(observer_location_center_point);
 
-    // observe_centerline::get_observation_locations();
-    // observe_centerline::output_observer_theoretical_x();
-    // ObserverBody fluid_observer_centerline(sph_system, "FluidObserverCenterline");
-    // fluid_observer_centerline.generateParticles<ObserverParticles>(observe_centerline::observation_location);
+    observe_centerline::get_observation_locations();
+    observe_centerline::output_observer_theoretical_pos_on_line();
+    ObserverBody fluid_observer_centerline(sph_system, "FluidObserverCenterline");
+    fluid_observer_centerline.generateParticles<ObserverParticles>(observe_centerline::observation_location);
 
-    // observe_cross_sections::getPositionsOfMultipleObserveLines();
-    // observe_cross_sections::output_observe_positions();
-    // observe_cross_sections::output_observe_theoretical_y();
-    // observe_cross_sections::output_number_observe_points_on_lines();
-    // ObserverBody fluid_observer_cross_section(sph_system, "FluidObserverCrossSections");
-    // fluid_observer_cross_section.generateParticles<ObserverParticles>(observe_cross_sections::observation_locations);
+    observe_cross_sections::getPositionsOfMultipleObserveLines();
+    observe_cross_sections::output_observe_positions();
+    observe_cross_sections::output_observer_theoretical_pos_on_line();
+    observe_cross_sections::output_number_observe_points_on_lines();
+    ObserverBody fluid_observer_cross_section(sph_system, "FluidObserverCrossSections");
+    fluid_observer_cross_section.generateParticles<ObserverParticles>(observe_cross_sections::observation_locations);
 
     /** topology */
     InnerRelation water_block_inner(water_block);
     ContactRelation water_wall_contact(water_block, {&wall_boundary});
-    // ContactRelation fluid_observer_centerline_contact(fluid_observer_centerline, {&water_block});
-    // ContactRelation fluid_observer_cross_section_contact(fluid_observer_cross_section, {&water_block});
+    ContactRelation fluid_observer_centerline_contact(fluid_observer_centerline, {&water_block});
+    ContactRelation fluid_observer_cross_section_contact(fluid_observer_cross_section, {&water_block});
     ContactRelation observer_centerpoint_contact(observer_center_point, {&water_block});
     //----------------------------------------------------------------------
     // Combined relations built from basic relations
@@ -207,9 +207,9 @@ int main(int ac, char *av[])
     body_states_recording.addToWrite<int>(water_block, "Indicator");            // output for debug
     body_states_recording.addToWrite<Real>(water_block, "Density");             // output for debug
     body_states_recording.addToWrite<Vecd>(water_block, "ZeroGradientResidue"); // output for debug
-    // ObservedQuantityRecording<Vecd> write_recorded_water_velocity("Velocity", fluid_observer_centerline_contact);
-    // ObservedQuantityRecording<Real> write_recorded_water_centerline_pressure("Pressure", fluid_observer_centerline_contact);
-    // ObservedQuantityRecording<Vecd> write_recorded_water_velocity_cross_section("Velocity", fluid_observer_cross_section_contact);
+    ObservedQuantityRecording<Vecd> write_recorded_water_centerline_velocity("Velocity", fluid_observer_centerline_contact);
+    ObservedQuantityRecording<Real> write_recorded_water_centerline_pressure("Pressure", fluid_observer_centerline_contact);
+    ObservedQuantityRecording<Vecd> write_recorded_water_velocity_cross_section("Velocity", fluid_observer_cross_section_contact);
     body_states_recording.addToWrite<int>(water_block, "BufferParticleIndicator");
     body_states_recording.addToWrite<Real>(water_block, "VolumetricMeasure");
     body_states_recording.addToWrite<Matd>(water_block, "LinearGradientCorrectionMatrix");
@@ -333,8 +333,8 @@ int main(int ac, char *av[])
             }
             water_block.updateCellLinkedList();
             water_block_complex.updateConfiguration();
-            // fluid_observer_centerline_contact.updateConfiguration();
-            // fluid_observer_cross_section_contact.updateConfiguration();
+            fluid_observer_centerline_contact.updateConfiguration();
+            fluid_observer_cross_section_contact.updateConfiguration();
 
             /** Tag truncated inlet/outlet particles*/
             inlet_outlet_surface_particle_indicator.exec();
@@ -342,12 +342,12 @@ int main(int ac, char *av[])
             left_bidirection_buffer.tag_buffer_particles.exec();
             right_bidirection_buffer.tag_buffer_particles.exec();
 
-            // if (physical_time > cutoff_time)
-            // {
-            //     write_recorded_water_velocity.writeToFile(number_of_iterations);
-            //     write_recorded_water_centerline_pressure.writeToFile(number_of_iterations);
-            //     write_recorded_water_velocity_cross_section.writeToFile(number_of_iterations);
-            // }
+            if (physical_time > cutoff_time)
+            {
+                write_recorded_water_centerline_velocity.writeToFile(number_of_iterations);
+                write_recorded_water_centerline_pressure.writeToFile(number_of_iterations);
+                write_recorded_water_velocity_cross_section.writeToFile(number_of_iterations);
+            }
             //if (physical_time > end_time * 0.5)
             //body_states_recording.writeToFile();
         }
