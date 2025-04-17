d368463ccd34ea1ba57680ef66c122e0ecea6af3
feng_wang_aer <feng.wang.aer@tum.de>
2025-04-18 01:31:35 +0200

    23

--- tests/extra_source_and_tests/33/33.cpp
+++ tests/extra_source_and_tests/33/33.cpp
@@ -230,7 +230,7 @@ int main(int ac, char *av[])
     BodyStatesRecordingToVtp body_states_recording(sph_system);
     //body_states_recording.addToWrite<Real>(water_block, "Pressure");            // output for debug
     //body_states_recording.addToWrite<int>(water_block, "Indicator");            // output for debug
-    //body_states_recording.addToWrite<Real>(water_block, "Density");             // output for debug
+    body_states_recording.addToWrite<Real>(water_block, "Density");             // output for debug
     //body_states_recording.addToWrite<Vecd>(water_block, "ZeroGradientResidue"); // output for debug
     ObservedQuantityRecording<Vecd> write_recorded_water_centerline_velocity("Velocity", fluid_observer_centerline_contact);
     ObservedQuantityRecording<Real> write_recorded_water_centerline_pressure("Pressure", fluid_observer_centerline_contact);
@@ -264,7 +264,7 @@ int main(int ac, char *av[])
     Real end_time = 1.8;                        /**< End time. */
     Real cutoff_ratio = 0.92;                   //** cutoff_time should be a integral and the same as the PY script */
     Real cutoff_time = cutoff_ratio * end_time; //** cutoff_time should be a integral and the same as the PY script */
-    Real num_output_files = 500.0;
+    Real num_output_files = 200.0;
     Real Output_Time = end_time / num_output_files; /**< Time stamps for output of body states. */
     Real index_check_file_fully_developed = num_output_files * cutoff_ratio;
     Real dt = 0.0; /**< Default acoustic time step sizes. */
