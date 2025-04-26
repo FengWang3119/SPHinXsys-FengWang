e7a67e4e7c310836f09abaf695f45176f3d8e4a4
Feng Wang <feng.wang.aer@tum.de>
2025-04-26 19:17:58 +0200

    1

--- tests/extra_source_and_tests/36/36.cpp
+++ tests/extra_source_and_tests/36/36.cpp
@@ -285,7 +285,10 @@ int main(int ac, char *av[])
         /** Integrate time (loop) until the next output time. */
         while (integration_time < Output_Time)
         {
-            apply_startup_gravity_force.exec();
+            if (physical_time <= 2.0)
+            {
+                apply_startup_gravity_force.exec();
+            }
 
             //Real Dt = get_fluid_advection_time_step_size.exec();
             Real Dt = get_turbulent_fluid_advection_time_step_size.exec();
