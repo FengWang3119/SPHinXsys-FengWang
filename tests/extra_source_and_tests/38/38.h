8db8ae828c045dfc5df7d528d282db8a2a1630a6
Feng Wang <feng.wang.aer@tum.de>
2025-05-11 10:38:38 +0200

    delete unnecessary comments

--- tests/extra_source_and_tests/38/38.h
+++ tests/extra_source_and_tests/38/38.h
@@ -30,25 +30,6 @@ Real resolution_ref = round(resolution_ref_temp * 1.0e8) / 1.0e8;
 Real offset_distance = y_p_constant - resolution_ref / 2.0; //%% Basically offset distance is large than or equal to 0 *
 Real BW = resolution_ref * 4;
 
-// Real extend_in = 2.0;
-// Real extend_out = 4.0;
-// Real extend_compensate_relaxation = 0.0;
-// Real DL1 = 1.0 + extend_in;
-// Real DL2 = 1.5;
-// Real DL3 = 1.0;
-// Real DL4 = 1.5;
-// Real DL5 = 1.0 + extend_out;
-// Real DL = DL1 + DL2 + DL3 + DL4 + DL5;
-// Real incline_angle = 30.0 * (2.0 * Pi / 360.0);
-// Real DH1 = DL2 * tan(incline_angle);
-// Vec2d point_A(0.0, DH);
-// Vec2d point_B(DL, DH);
-// Vec2d point_C(DL, 0.0);
-// Vec2d point_D(DL - DL5, 0.0);
-// Vec2d point_E(DL - DL5 - DL4, DH1);
-// Vec2d point_F(DL1 + DL2, DH1);
-// Vec2d point_G(DL1, 0.0);
-
 Vec2d point_O(0.0, 0.0);
 
 Vec2d point_1(2.42139, 0.95617);
@@ -95,15 +76,6 @@ StdVec<Real> initial_turbu_values = {0.000180001, 3.326679e-5, 1.0e-3};
 //----------------------------------------------------------------------
 //	Material properties of the fluid.
 //----------------------------------------------------------------------
-//** Laminar *
-//Real U_inlet = 0.5;
-//Real U_max = 0.75;
-//Real U_f = U_inlet; //*Characteristic velo is regarded as average velo here
-//Real c_f = 10.0 * U_max;                                        /**< Speed of sound. */
-//Real rho0_f = 1.0;                                            /**< Density. */
-//Real mu_f = 0.01;
-
-//** Same parameter as SPH_4 *
 //Real U_inlet = 1.0;
 //Real Outlet_pressure = 0.0;
 //Real U_f = U_inlet;         //*Characteristic velocity
@@ -129,40 +101,6 @@ Real mu_f = 1.0e-6; //** Periodic BC */
 Real Re_calculated = U_f * DH * rho0_f / mu_f;
 
 Real DH_C = DH - 2.0 * offset_distance;
-
-//** Initial inlet pressure to drive flow *
-//Real initial_inlet_pressure = 0.5 * rho0_f * U_inlet * U_inlet;
-//Real initial_inlet_pressure = 5.0;
-//----------------------------------------------------------------------
-//	The emitter block with offset model.
-//----------------------------------------------------------------------
-/*
-Real BW = resolution_ref * 4; /% Reference size of the emitter. /
-Real DL_sponge = resolution_ref * 20;
-Real half_channel_height = DH / 2.0;
-
-Vec2d left_buffer_halfsize = Vec2d(0.5 * BW, 0.5 * DH_C + BW);
-Vec2d left_buffer_translation = Vec2d(-DL_sponge, 0.0) + left_buffer_halfsize + Vecd(0.0, offset_distance - BW);
-
-Real outlet_buffer_length = BW;
-Real outlet_buffer_height = DH_C + 2.0 * BW;
-
-Real outlet_disposer_rotation_angel = 0.0; //%% By default, counter-clockwise is positive *
-Vec2d outlet_buffer_center_translation = (point_B + point_C) / 2.0 + Vecd(-1.0, 0.0) * outlet_buffer_length / 2.0;
-
-Real outlet_emitter_rotation_angel = Pi + outlet_disposer_rotation_angel; //%% By default, counter-clockwise is positive *
-
-//% If return to the straight channel 
-// Real outlet_disposer_rotation_angel = 0.0 * Pi ; //%% By default, counter-clockwise is positive *
-// Real outlet_emitter_rotation_angel =  Pi ; //%% By default, counter-clockwise is positive *
-// Vec2d outlet_buffer_center_translation = Vec2d(DL_domain - 0.5 * outlet_buffer_length , 0.5 * DH) ;
-
-Vec2d right_buffer_halfsize = Vec2d(0.5 * outlet_buffer_length, 0.75 * outlet_buffer_height);
-Vec2d right_buffer_translation = outlet_buffer_center_translation;
-
-//Vec2d disposer_halfsize = Vec2d(0.75 * DH, 0.5 * BW);
-//Vec2d disposer_translation = Vec2d(DL_domain + 0.25 * DH, DH_domain ) - disposer_halfsize;
-*/
 
 //----------------------------------------------------------------------
 //	Domain bounds of the system.
@@ -285,27 +223,6 @@ void output_number_observe_points_on_lines()
 //----------------------------------------------------------------------
 //	Cases-dependent geometries
 //----------------------------------------------------------------------
-/*
-std::vector<Vecd> createWaterBlockShape()
-{
-    std::vector<Vecd> water_block_shape;
-
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - extend_compensate_relaxation, 0.0));
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - extend_compensate_relaxation, DH));
-    water_block_shape.push_back(point_A);
-    water_block_shape.push_back(point_B);
-    water_block_shape.push_back(Vecd(point_B[0] + offset_distance, point_B[1]));
-    water_block_shape.push_back(Vecd(point_C[0] + offset_distance, point_C[1]));
-    water_block_shape.push_back(point_C);
-    water_block_shape.push_back(point_D);
-    water_block_shape.push_back(point_E);
-    water_block_shape.push_back(point_F);
-    water_block_shape.push_back(point_G);
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - extend_compensate_relaxation, 0.0));
-
-    return water_block_shape;
-}
-*/
 std::vector<Vecd> createWaterBlockShape()
 {
     std::vector<Vecd> water_block_shape;
@@ -388,7 +305,6 @@ class WaterBlock : public ComplexShape
     explicit WaterBlock(const std::string &shape_name) : ComplexShape(shape_name)
     {
         MultiPolygon computational_domain(createWaterBlockShape());
-        //add<ExtrudeShape<MultiPolygonShape>>(-offset_distance, computational_domain, "ComputationalDomain");
         add<MultiPolygonShape>(computational_domain, "ComputationalDomain");
 
         MultiPolygon sub_upper_dummy_boundary(createHookWallShape());
@@ -397,46 +313,6 @@ class WaterBlock : public ComplexShape
         subtract<MultiPolygonShape>(sub_bottom_dummy_boundary, "SubBottomDummyBoundary");
     }
 };
-/*
-std::vector<Vecd> createOuterWallShape()
-{
-    std::vector<Vecd> water_block_shape;
-
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - BW, 0.0));
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - BW, DH));
-    water_block_shape.push_back(point_A);
-    water_block_shape.push_back(point_B);
-    water_block_shape.push_back(Vecd(point_B[0] + offset_distance + BW, point_B[1]));
-    water_block_shape.push_back(Vecd(point_C[0] + offset_distance + BW, point_C[1]));
-    water_block_shape.push_back(point_C);
-    water_block_shape.push_back(point_D);
-    water_block_shape.push_back(point_E);
-    water_block_shape.push_back(point_F);
-    water_block_shape.push_back(point_G);
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - BW, 0.0));
-
-    return water_block_shape;
-}
-std::vector<Vecd> createInnerWallShape()
-{
-    std::vector<Vecd> water_block_shape;
-
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - 2.0 * BW, 0.0));
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - 2.0 * BW, DH));
-    water_block_shape.push_back(point_A);
-    water_block_shape.push_back(point_B);
-    water_block_shape.push_back(Vecd(point_B[0] + offset_distance + 2.0 * BW, point_B[1]));
-    water_block_shape.push_back(Vecd(point_C[0] + offset_distance + 2.0 * BW, point_C[1]));
-    water_block_shape.push_back(point_C);
-    water_block_shape.push_back(point_D);
-    water_block_shape.push_back(point_E);
-    water_block_shape.push_back(point_F);
-    water_block_shape.push_back(point_G);
-    water_block_shape.push_back(Vecd(-DL_sponge - offset_distance - 2.0 * BW, 0.0));
-
-    return water_block_shape;
-}
-*/
 
 /**
  * @brief 	Wall boundary body definition.
@@ -446,71 +322,9 @@ class WallBoundary : public ComplexShape
   public:
     explicit WallBoundary(const std::string &shape_name) : ComplexShape(shape_name)
     {
-        // MultiPolygon outer_dummy_boundary(createOuterWallShape());
-        // add<ExtrudeShape<MultiPolygonShape>>(-offset_distance + BW, outer_dummy_boundary, "OuterDummyBoundary");
-
-        // MultiPolygon inner_dummy_boundary(createInnerWallShape());
-        // subtract<ExtrudeShape<MultiPolygonShape>>(-offset_distance, inner_dummy_boundary, "InnerDummyBoundary");
         MultiPolygon upper_dummy_boundary(createUpperWallShape());
         add<MultiPolygonShape>(upper_dummy_boundary, "UpperDummyBoundary");
         MultiPolygon bottom_dummy_boundary(createBottomWallShape());
         add<MultiPolygonShape>(bottom_dummy_boundary, "BottomDummyBoundary");
     }
 };
-
-/*
-//----------------------------------------------------------------------
-//	Inflow velocity
-//----------------------------------------------------------------------
-struct InflowVelocity
-{
-    Real u_ref_, t_ref_;
-    AlignedBox &aligned_box_;
-    Vecd halfsize_;
-
-    template <class BoundaryConditionType>
-    InflowVelocity(BoundaryConditionType &boundary_condition)
-        : u_ref_(U_inlet), t_ref_(2.0),
-          aligned_box_(boundary_condition.getAlignedBox()),
-          halfsize_(aligned_box_.HalfSize()) {}
-
-    Vecd operator()(Vecd &position, Vecd &velocity, Real current_time)
-    {
-        Vecd target_velocity = velocity;
-        Real u_ave = current_time < t_ref_ ? 0.5 * u_ref_ * (1.0 - cos(Pi * current_time / t_ref_)) : u_ref_;
-        //target_velocity[0] = 1.5 * u_ave * SMAX(0.0, 1.0 - position[1] * position[1] / halfsize_[1] / halfsize_[1]);
-        //target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
-        target_velocity[0] = u_ave;
-        if (position[1] > half_channel_height)
-        {
-            std::cout << "Particles out of domain, wrong inlet velocity." << std::endl;
-            std::cout << position[1] << std::endl;
-            std::cin.get();
-        }
-        target_velocity[1] = 0.0;
-        return target_velocity;
-    }
-};
-
-struct RightOutflowPressure
-{
-    template <class BoundaryConditionType>
-    RightOutflowPressure(BoundaryConditionType &boundary_condition) {}
-
-    Real operator()(Real p, Real current_time)
-    {
-        Real pressure = Outlet_pressure;
-        return pressure;
-    }
-};
-struct LeftInflowPressure
-{
-    template <class BoundaryConditionType>
-    LeftInflowPressure(BoundaryConditionType &boundary_condition) {}
-
-    Real operator()(Real p, Real current_time)
-    {
-        return p;
-    }
-};
-*/
