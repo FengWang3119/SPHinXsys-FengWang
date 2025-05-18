7a661f8796b8b40e2937e3de15701832b4cb1b92
Feng Wang <feng.wang.aer@tum.de>
2025-05-18 13:03:27 +0200

    6-8

--- tests/extra_source_and_tests/38/38.cpp
+++ tests/extra_source_and_tests/38/38.cpp
@@ -20,7 +20,12 @@ int main(int ac, char *av[])
      */
 
     FluidBody water_block(sph_system, makeShared<WaterBlockExtruded>("WaterBody"));
-    water_block.defineBodyLevelSetShape();
+    if (sph_system.RunParticleRelaxation())
+    {
+        std::cout << "water_block.defineBodyLevelSetShape starts" << std::endl;
+        water_block.defineBodyLevelSetShape();
+        std::cout << "water_block.defineBodyLevelSetShape ends" << std::endl;
+    }
     water_block.defineClosure<WeaklyCompressibleFluid, Viscosity>(ConstructArgs(rho0_f, c_f), mu_f);
     ParticleBuffer<ReserveSizeFactor> inlet_particle_buffer(0.5);
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles() && !is_always_lattice_arrange_fluid)
@@ -30,7 +35,12 @@ int main(int ac, char *av[])
      * @brief 	Particle and body creation of wall boundary.
      */
     SolidBody wall_boundary(sph_system, makeShared<WallBoundary>("Wall"));
-    wall_boundary.defineBodyLevelSetShape();
+    if (sph_system.RunParticleRelaxation())
+    {
+        std::cout << "wall_boundary.defineBodyLevelSetShape starts" << std::endl;
+        wall_boundary.defineBodyLevelSetShape();
+        std::cout << "wall_boundary.defineBodyLevelSetShape ends" << std::endl;
+    }
     wall_boundary.defineMaterial<Solid>();
     (!sph_system.RunParticleRelaxation() && sph_system.ReloadParticles())
         ? wall_boundary.generateParticles<BaseParticles, Reload>(wall_boundary.getName())
@@ -170,6 +180,10 @@ int main(int ac, char *av[])
     //----------------------------------------------------------------------
     // Periodic BC
     //----------------------------------------------------------------------
+    // FluidBody water_block_extruded(sph_system, makeShared<WaterBlockExtruded>("WaterBodyExtruded"));
+    // water_block_extruded.defineClosure<WeaklyCompressibleFluid, Viscosity>(ConstructArgs(rho0_f, c_f), mu_f);
+    // water_block_extruded.generateParticles<BaseParticles, Lattice>();
+
     PeriodicAlongAxis periodic_along_x(water_block.getSPHBodyBounds(), xAxis);
     PeriodicConditionUsingCellLinkedList periodic_condition_x(water_block, periodic_along_x);
 
@@ -288,6 +302,8 @@ int main(int ac, char *av[])
             transport_velocity_correction.exec();
             get_limiter_of_transport_velocity_correction.exec();
 
+            kernel_summation.exec();
+
             Real relaxation_time = 0.0;
             int inner_itr = 0;
             while (relaxation_time < Dt)
@@ -304,8 +320,6 @@ int main(int ac, char *av[])
                     turbulent_kinetic_energy_force.exec();
                 }
                 pressure_relaxation.exec(dt);
-
-                kernel_summation.exec();
 
                 if (is_constrain_normal_velocity_in_P_region)
                 {
