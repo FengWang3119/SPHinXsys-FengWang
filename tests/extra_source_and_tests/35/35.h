20b996d5af9aed8863f6800488697a4f0f233d34
Feng Wang <feng.wang.aer@tum.de>
2025-05-23 11:48:23 +0200

    14

--- tests/extra_source_and_tests/35/35.h
+++ tests/extra_source_and_tests/35/35.h
@@ -115,6 +115,7 @@ Vecd point_7(-5.5, -7.0, 0.5);
 Vecd point_8(1.06066, -8.83883, 0.5);
 Vecd point_out(11.9373, 0.0, 3.5);
 */
+
 /*
 //% For G4
 Vecd point_1(7.0, -6.0, 0.5);
@@ -127,6 +128,8 @@ Vecd point_7(-6.0, -7.0, 0.5);
 Vecd point_8(0.707107, -9.19239, 0.5);
 Vecd point_out(11.9373, 0.0, 3.5);
 */
+
+/*
 //% For G5
 Vecd point_1(7.0, -6.0, 0.5);
 Vecd point_2(9.19239, 0.707107, 0.5);
@@ -137,6 +140,20 @@ Vecd point_6(-9.19239, -0.707107, 0.5);
 Vecd point_7(-6.0, -7.0, 0.5);
 Vecd point_8(0.707107, -9.19239, 0.5);
 Vecd point_out(8.93725, 0.0, 3.5);
+*/
+
+///*
+//% For G6
+Vecd point_1(7.0, -6.0, 0.5);
+Vecd point_2(9.19239, 0.707107, 0.5);
+Vecd point_3(6.0, 7.0, 0.5);
+Vecd point_4(-0.707107, 9.19239, 0.5);
+Vecd point_5(-7.0, 6.0, 0.5);
+Vecd point_6(-9.19239, -0.707107, 0.5);
+Vecd point_7(-6.0, -7.0, 0.5);
+Vecd point_8(0.707107, -9.19239, 0.5);
+Vecd point_out(9.93725, 0.0, 3.5);
+//*/
 
 Vecd axis_vector_x(1.0, 0.0, 0.0);
 Vecd axis_vector_z(0.0, 0.0, 1.0);
@@ -221,7 +238,7 @@ Vecd outlet_sub_buffer_translation = outlet_buffer_translation + buffer_thicknes
 //----------------------------------------------------------------------
 //	Cases-dependent geometries
 //----------------------------------------------------------------------
-std::string stl_fluid_path = "./input/g5.stl";
+std::string stl_fluid_path = "./input/g6.stl";
 Real scale_factor_fluid = 1.0;
 Vecd translation_stl_fluid(0.0, 0.0, 0.0);
 class WaterBlock : public ComplexShape
@@ -234,7 +251,7 @@ class WaterBlock : public ComplexShape
 };
 
 /** Set the file path to the stl file. */
-std::string stl_structure_path = "./input/g5.stl"; //% This also denote which file we use
+std::string stl_structure_path = "./input/g6.stl"; //% This also denote which file we use
 Real scale_factor = 1.0;
 Vecd translation_stl(0.0, 0.0, 0.0);
 class WallBoundaryFromSTL : public ComplexShape
