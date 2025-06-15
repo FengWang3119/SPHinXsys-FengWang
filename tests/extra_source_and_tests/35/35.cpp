c1f32519520230bc8cd2ebd7ba440f59821dd98c
Feng Wang <feng.wang.aer@tum.de>
2025-06-15 11:05:26 +0200

    32

--- tests/extra_source_and_tests/35/35.cpp
+++ tests/extra_source_and_tests/35/35.cpp
@@ -341,10 +341,10 @@ int main(int ac, char *av[])
     WriteToVtpIfVelocityOutOfBound abnormal_velocity_recording(sph_system, 1.0e6 * U_max);
 
     //% Temporary Treat Note that this should be in front of TAG particle include inlet outlet and buffer
-    SimpleDynamics<DisposerForInitialParticleDeletion> delete_initial_particle(water_block);
-    delete_initial_particle.exec();
-    particle_sorting.exec();
-    water_block.updateCellLinkedList();
+    // SimpleDynamics<DisposerForInitialParticleDeletion> delete_initial_particle(water_block);
+    // delete_initial_particle.exec();
+    // particle_sorting.exec();
+    // water_block.updateCellLinkedList();
 
     initialise_color_indicator.exec();
     /**
