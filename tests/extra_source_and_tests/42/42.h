e1f9b465085c1a49375fffb2e0ef08d7177a00e4
Feng-Koshilab <feng.wang.aer@tum.de>
2025-09-16 14:49:39 +0900

    8

--- tests/extra_source_and_tests/42/42.h
+++ tests/extra_source_and_tests/42/42.h
@@ -33,7 +33,7 @@ Real relaxation_rate_turbulent_inlet = 0.8;
 int is_AMRD = 1;
 bool is_constrain_normal_velocity_in_P_region = false;
 //** Weight for correcting the velocity  gradient in the sub near wall region  *
-Real weight_vel_grad_sub_nearwall = 0.5;
+Real weight_vel_grad_sub_nearwall = 0.1;
 //** Tag for Source Term Linearisation *
 bool is_source_term_linearisation = false;
 
