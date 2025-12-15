96db4420b879e272b780c6b90dd304ca4be85799
feng_wang_aer <feng.wang.aer@tum.de>
2025-12-16 00:30:32 +0100

    32

--- tests/extra_source_and_tests/33/33.cpp
+++ tests/extra_source_and_tests/33/33.cpp
@@ -241,7 +241,7 @@ int main(int ac, char *av[])
     //body_states_recording.addToWrite<Real>(water_block, "VolumetricMeasure");
     //body_states_recording.addToWrite<Matd>(water_block, "LinearGradientCorrectionMatrix");
     
-    WriteToVtpIfVelocityOutOfBound abnornal_velocity_recording(sph_system, 10);
+    WriteToVtpIfVelocityOutOfBound abnornal_velocity_recording(sph_system, 15);
     /**
      * @brief Setup geometry and initial conditions.
      */
