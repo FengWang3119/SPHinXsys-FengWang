5f9a21c3ffc184b9329376ccbd7275a74c5bb64e
Feng <Feng@DESKTOP-NACNAHC>
2026-01-14 17:42:22 +0100

    31

--- tests/extra_source_and_tests/extra_src/shared/pressure_boundary/bidirectional_buffer.h
+++ tests/extra_source_and_tests/extra_src/shared/pressure_boundary/bidirectional_buffer.h
@@ -98,7 +98,7 @@ class BidirectionalBuffer
               p_(particles_->getVariableDataByName<Real>("Pressure")),
               previous_surface_indicator_(particles_->getVariableDataByName<int>("PreviousSurfaceIndicator")),
               buffer_particle_indicator_(particles_->getVariableDataByName<int>("BufferParticleIndicator")),
-              upper_bound_fringe_(0.75 * sph_body_.getSPHBodyResolutionRef()),
+              upper_bound_fringe_(1.0 * sph_body_.getSPHBodyResolutionRef()),
               physical_time_(sph_system_.getSystemVariableDataByName<Real>("PhysicalTime")),
               target_pressure_(target_pressure)
         {
