8a2f02dc078f26c11ff171bb240eebd98321a339
Feng <Feng@DESKTOP-NACNAHC>
2026-01-23 22:40:42 +0100

    fix a bug where type_turbulent_inlet in 19.h does not work actually

--- tests/extra_source_and_tests/19/19.h
+++ tests/extra_source_and_tests/19/19.h
@@ -304,7 +304,7 @@ struct InflowVelocity
         //target_velocity[0] = 1.5 * u_ave * SMAX(0.0, 1.0 - position[1] * position[1] / halfsize_[1] / halfsize_[1]);
         //target_velocity[0] = 1.5 * u_ave * (1.0 - position[1] * position[1] / half_channel_height / half_channel_height);
         target_velocity[0] = u_ave;
-        if (1)
+        if (type_turbulent_inlet == 1)
         {
             //** Impose fully-developed velocity from PYTHON result */
             //** Calculate the distance to wall, Y. position[1] is the distance to the centerline */
