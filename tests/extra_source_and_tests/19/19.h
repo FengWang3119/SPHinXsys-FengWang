3de954d5de448ce9006cd0e708b16d90ddb0178f
Feng <Feng@DESKTOP-NACNAHC>
2026-01-30 10:54:40 +0100

    82

--- tests/extra_source_and_tests/19/19.h
+++ tests/extra_source_and_tests/19/19.h
@@ -142,11 +142,11 @@ constexpr const char *namespace_prefix = "nearwall";
 const int number_observe_line = 1;
 Real sparse_ratio = 2.0;
 Real observer_offset_distance = 0.0 * resolution_ref; //** Offset the first and last observing point *
-Real observer_offset_distance_whole_line = 2.0 * resolution_ref;
+Real observer_offset_distance_whole_line = 0.0 * resolution_ref;
 Vec2d unit_direction_observe(1.0, 0.0);
 // ** Determine the observing start point of the each line. *
 Real observe_start_x[number_observe_line] = {0.0};
-Real observe_start_y[number_observe_line] = {0.5 * resolution_ref - observer_offset_distance_whole_line};
+Real observe_start_y[number_observe_line] = {0.0 * resolution_ref - observer_offset_distance_whole_line};
 // ** Determine the length of the observing line and other information. *
 Real observe_line_length[number_observe_line] = {0.0};
 int num_observer_points[number_observe_line] = {0};
