2259445f943fa88cfc9633eac6e068717d98b4f4
Feng <Feng@DESKTOP-NACNAHC>
2026-03-12 11:55:42 +0100

    11

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_common_turbulence_model.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_common_turbulence_model.cpp
@@ -359,10 +359,10 @@ void TurbuViscousForce<Contact<Wall>>::interaction(size_t index_i, Real dt)
             Real u_star_j = get_dimensionless_velocity(y_star_j, current_time, u_star_previous, is_blended_[index_i]);
             Real fric_vel_mag_j = sqrt(C_mu_wf_25_ * turbu_k_i_05 * vel_i_tau_mag / u_star_j);
 
-            //if (is_near_wall_P1_[index_i] == 1)
-            //{
-            //    fric_vel_mag_j = friction_velocity_from_sublayer_[index_i];
-            //}
+            if (is_near_wall_P1_[index_i] == 1)
+            {
+                fric_vel_mag_j = friction_velocity_from_sublayer_[index_i];
+            }
 
             //** Construct local wall shear stress, if this is on each wall particle j   *
             Real WSS_tn_mag_j = rho_i * fric_vel_mag_j * fric_vel_mag_j * boost::qvm::sign(vel_i.dot(e_j_tau));
