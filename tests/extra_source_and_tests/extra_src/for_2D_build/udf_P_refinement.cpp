6d595efd834069ca249163424ea0e21f2258fa3a
Feng <Feng@DESKTOP-NACNAHC>
2026-03-12 22:36:08 +0100

    13

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
@@ -110,7 +110,7 @@ namespace udf
             Real flow_rate_whole = u_outer * fluid_particle_spacing_;
             Real flow_rate_local = flow_rate_whole - flow_rate_half;
 
-            flow_rate_local = u_outer * 0.5 * fluid_particle_spacing_ * 0.5;
+            flow_rate_local = 3.781607e-3;
             
             friction_velocity_from_sublayer_[index_i] = solve_1D_sublayer(nu, u_outer, k_outer, omega_outer, std::abs(dudn),
                 nut_outer, distance_to_wall, friction_vel_magnitude, std::abs(flow_rate_local));
