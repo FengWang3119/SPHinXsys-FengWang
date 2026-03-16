8b63a43e07714fb2696fbfebf88b7957cd9b7649
Feng <Feng@DESKTOP-NACNAHC>
2026-03-16 12:05:34 +0100

    25

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
@@ -131,6 +131,7 @@ namespace udf
             omega_outer = 5.998469e+1;
             nut_outer = 1.885024e-5;
             u_outer = 3.000607e-1;
+            nu = 3.5e-4;
 
             node_value_[index_i] = solve_1D_sublayer(nu, u_outer, k_outer, omega_outer, std::abs(dudn),
                 nut_outer, distance_to_wall, friction_vel_magnitude, std::abs(flow_rate_local));
