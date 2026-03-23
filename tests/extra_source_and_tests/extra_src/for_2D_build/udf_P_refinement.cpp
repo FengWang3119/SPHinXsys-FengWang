2dbd72c2d40f936cb3c2095821c9057c513a0e49
Feng <Feng@DESKTOP-NACNAHC>
2026-03-23 11:30:44 +0100

    64

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
@@ -151,12 +151,12 @@ namespace udf
             Real dudn = 0.0;
 
             //** If use full from SPH *
-            dudn = dudn_from_SPH;
+            //dudn = dudn_from_SPH;
 
             //** If use weighting combination *
-            //Real weight_SPH = 0.5 * fluid_particle_spacing_;
-            //Real sum_weight_sublayer = distance_to_wall; //** Assume uniform division *
-            //dudn = (dudn_from_SPH * weight_SPH + sum_node_vel_difference) / (weight_SPH + sum_weight_sublayer); 
+            Real weight_SPH = 0.5 * fluid_particle_spacing_;
+            Real sum_weight_sublayer = distance_to_wall; //** Assume uniform division *
+            dudn = (dudn_from_SPH * weight_SPH + sum_node_vel_difference) / (weight_SPH + sum_weight_sublayer); 
 
             Real nut_outer = turbu_mu_[index_i] / rho_[index_i];
             
