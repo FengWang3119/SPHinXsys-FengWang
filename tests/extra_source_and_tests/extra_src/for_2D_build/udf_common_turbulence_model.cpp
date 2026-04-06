40672058082d7bee85c48a52b8999e576ca55231
Feng <Feng@DESKTOP-NACNAHC>
2026-04-06 21:33:03 +0200

    105

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_common_turbulence_model.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_common_turbulence_model.cpp
@@ -306,10 +306,13 @@ void TurbuViscousForce<Inner<>>::interaction(size_t index_i, Real dt)
         }
 
         //** P-refinement for S and P particle, not using ARD, and impose SS from sublayer *
-        if (this->is_near_wall_P2_[index_i] == 10)
+        if (is_near_wall_P1_[index_i] == 1)
         {
-            Matd shear_stress_sublayer = mu_eff_i * (dUdn_P_sublayer_[index_i] + dUdn_P_sublayer_[index_i].transpose());
-            shear_stress_eij_corrected = shear_stress_sublayer * e_ij;
+            if (is_near_wall_P1_[index_j] == 1)
+            {
+                Matd shear_stress_sublayer = mu_eff_i * (dUdn_P_sublayer_[index_i] + dUdn_P_sublayer_[index_i].transpose());
+                shear_stress_eij_corrected = shear_stress_sublayer * e_ij;
+            }
         }
 
         shear_stress = (shear_stress - shear_stress_eij) + shear_stress_eij_corrected;
