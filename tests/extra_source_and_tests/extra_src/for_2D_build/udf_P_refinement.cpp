1239205dec825e04fa23df1f97713f4f2b818a6c
Feng <Feng@DESKTOP-NACNAHC>
2026-04-16 16:43:05 +0200

    156

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.cpp
@@ -508,8 +508,8 @@ namespace udf
         double Q_target, double k_grad_p_outer, double w_grad_p_outer, double& vel_nodeO, double& vel_nodeUM)
     {
         //------------------------------------------------¡ý Input parameters ¡ý------------------------------------------------
-        constexpr int ny = 10; // Manually determine
-        num_sub_node_ = 10; //** Temp *
+        constexpr int ny = 5; // Manually determine
+
         if (num_sub_node_ != ny)
         {
             std::cout << "Node number mismatch! Stop here." << std::endl;
@@ -535,7 +535,7 @@ namespace udf
         //** For 1D verification *
         bool output_detailed_info = true;
         int NF = 40;
-        int index = 155;
+        int index = 156;
 
         double flow_rate_target = Q_target;
         double utau = utau_init;
@@ -682,7 +682,7 @@ namespace udf
             dwdy[ny - 1] = (w_nodeUM - turbu_omega_star[ny - 2]) / (2.0 * hy);
             dudy_discretized_backward[ny - 1] = (u_star[ny - 1] - u_star[ny - 2]) / hy; //** No need for nodeUM *
             //** Transfer *
-            vel_grad_nodeO = dudy_discretized_central[ny - 1];
+            vel_grad_nodeO = dudy_discretized_backward[ny - 1];
             //vel_grad_nodeO = vel_grad_p_outer;
             //------------------------------------------------¡ü Calculate gradients of u, k, omega, Dk, Dw ¡ü------------------------------------------------
 
@@ -768,8 +768,7 @@ namespace udf
             d_u[last] = pressure_gradient_constant * hy * hy + nu_eff_last_plus_half * u_nodeUM;
             // solving
             double U_new[ny]{};
-            //tdma5(a_u, b_u, c_u, d_u, U_new);
-            tdma(ny, a_u, b_u, c_u, d_u, U_new);
+            tdma5(a_u, b_u, c_u, d_u, U_new);
             //-------------------------------------¡ü For velocity ¡ü-------------------------------------
 
             //-------------------------------------¡ý For turbulent kinetic energy ¡ý-------------------------------------
@@ -810,8 +809,7 @@ namespace udf
             d_k[last] = hy * hy * nut_star[last] * dudy_star[last] * dudy_star[last] + Dk_last_plus_half * k_nodeUM;
             // solving
             double K_new[ny]{};
-            //tdma5(a_k, b_k, c_k, d_k, K_new);
-            tdma(ny, a_k, b_k, c_k, d_k, K_new);
+            tdma5(a_k, b_k, c_k, d_k, K_new);
             // avoid negative value, K_new = max(K_new, k_min)
             double k_min = 1e-10;
             for (int i = 0; i < ny; ++i) {
@@ -856,8 +854,7 @@ namespace udf
             d_w[last] = hy * hy * (part_production_last + part_cross_diffusion[last]) + Dw_last_plus_half * w_nodeUM;
             // solving
             double Turbu_omega_new[ny]{};
-            //tdma5(a_w, b_w, c_w, d_w, Turbu_omega_new);
-            tdma(ny, a_w, b_w, c_w, d_w, Turbu_omega_new);
+            tdma5(a_w, b_w, c_w, d_w, Turbu_omega_new);
             // avoid negative value, Turbu_omega_new = max(Turbu_omega_new, omega_min)
             double omega_min = 1e-10;
             for (int i = 0; i < ny; ++i) {
