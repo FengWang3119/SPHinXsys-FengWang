2d948f9518f0a0a95150bae27c0aaf071f51e154
Feng <Feng@DESKTOP-NACNAHC>
2026-06-10 01:38:31 +0200

    20

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.h
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.h
@@ -80,7 +80,7 @@ namespace udf
         void test_sublayer_model_half_channel_height();
         
         //** Locally effective, mannually set number of node *
-        static constexpr int ny = 4; //** Currently only Vec6d can be used, so ny should <=5, other space will be zero *
+        static constexpr int ny = 10; //** Currently only Vec6d can be used, so ny should <=5, other space will be zero *
         using Vec_ny_d = Eigen::Matrix<Real, ny, 1>;
         inline void check_num_node_consistency()
         {
