efba3ec42e6a087195bae0209542035e2e5fa54f
Feng <Feng@DESKTOP-NACNAHC>
2026-06-25 21:23:54 +0200

    29

--- tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.h
+++ tests/extra_source_and_tests/extra_src/for_2D_build/udf_P_refinement.h
@@ -81,7 +81,7 @@ namespace udf
         void test_sublayer_model_specific_channel_height();
         
         //** Locally effective, mannually set number of node *
-        static constexpr int ny = 20; //** Currently only Vec6d can be used, so ny should <=5, other space will be zero *
+        static constexpr int ny = 25; //** Currently only Vec6d can be used, so ny should <=5, other space will be zero *
         static constexpr int type_tdma_ = 0; //** 0: general TMDA, other numbers refer to corresponding unrolled version  *
 
         using Vec_ny_d = Eigen::Matrix<Real, ny, 1>;
