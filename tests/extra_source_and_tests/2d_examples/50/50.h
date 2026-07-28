5331c2ca71905566a4e772d5ef89c6249b126491
Feng-ROG-NUC <wangfeng3119@outlook.com>
2026-07-28 14:13:15 +0200

    9

--- tests/extra_source_and_tests/2d_examples/50/50.h
+++ tests/extra_source_and_tests/2d_examples/50/50.h
@@ -16,7 +16,7 @@ using namespace SPH;
 //----------------------------------------------------------------------
 Real DH = 2.0;  /**< Channel height. */
 Real DL = 30.0; /**< Channel length. */
-Real num_fluid_cross_section = 160.0;
+Real num_fluid_cross_section = 320.0;
 
 Real time_gradually_increase_vel = 2.0;
 //----------------------------------------------------------------------
